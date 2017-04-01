4727a5ae7c65452215881b0f320d1791c0b08891
Sejal <sejal.khatri5@gmail.com>
2017-04-02 01:38:22 +0530

    Working version

--- reconNeuro/app/controllers/dashboards_controller.rb
+++ reconNeuro/app/controllers/dashboards_controller.rb
@@ -1,18 +1,22 @@
 class DashboardsController < ApplicationController
+  before_action :set_object
   def index
-    @object_input = Neuro.where(id: 1)
+    if @object_input.nil?
+      @object_input = Neuro.new
+    end
   end
-
   def update
-   @object_input = Neuro.where(id: 1)
    if @object_input.update(object_params)
      flash[:notice] = 'Image updated'
-     redirect_to controller: 'dashboards', action: 'index'
+     render "dashboards/index"
    end
   end
 
   private
   def object_params
     params.require(:object_input).permit(:object1)
+  end
+  def set_object
+    @object_input = Neuro.where(id: 1).first
   end
 end
